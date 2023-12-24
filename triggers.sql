8f93e6a2d33f6f8aa12647e136b17f8a2e01d329
photonrays <lthang052@gmail.com>
2023-12-24 16:16:13 +0700

    sua lai cai trigger khoa chinh, them unique constraint

--- triggers.sql
+++ triggers.sql
@@ -1,5 +1,9 @@
 ﻿USE QuanLyShowroomOto;
 
+CREATE TEMPORARY TABLE IF NOT EXISTS debug_table (info varchar(200));
+
+SELECT * FROM debug_table dt;
+
 -- TRIGGERS --
 DROP TRIGGER IF EXISTS update_khi_nhap;
 
@@ -9,17 +13,19 @@ CREATE TRIGGER update_khi_nhap
 BEFORE INSERT ON CTPHIEUNHAP
 FOR EACH ROW
 BEGIN
-    DECLARE countMaXe INT;
+    DECLARE countXe INT;
 
-    SELECT COUNT(*) INTO countMaXe FROM XE WHERE MaXe = NEW.MaXe;
+    SELECT COUNT(*) INTO countXe FROM XE WHERE TenXe = NEW.TenXe;
+    INSERT INTO debug_table (info) VALUES (CONCAT('tenXe: ', NEW.TenXe, countXe));
 
-    IF countMaXe > 0 THEN
+
+    IF countXe > 0 THEN
         UPDATE XE
         SET SoLuong = SoLuong + NEW.SoLuong
-        WHERE MaXe = NEW.MaXe;
+        WHERE TenXe = NEW.TenXe;
     ELSE
-        INSERT INTO XE (MaXe, TenXe, LoaiXe, Hang, Gia, SoLuong, ThongSo)
-        VALUES (NEW.MaXe, NEW.TenXe, NEW.LoaiXe, NEW.Hang, NEW.Gia, NEW.SoLuong, NEW.ThongSo);
+        INSERT INTO XE (TenXe, LoaiXe, Hang, Gia, SoLuong, ThongSo)
+        VALUES (NEW.TenXe, NEW.LoaiXe, NEW.Hang, NEW.Gia, NEW.SoLuong, NEW.ThongSo);
     END IF;
 END;
 //
@@ -44,3 +50,7 @@ END;
 //
 
 DELIMITER ;
+
+INSERT INTO CTPHIEUNHAP (MaPN, TenXe, LoaiXe, Hang, Gia, SoLuong, ThongSo, ThanhTien)
+VALUES ('PN001', 'Xe A', 'Sedan', 'Honda', 50000, 2, 'Thong so A', 100000);
+
