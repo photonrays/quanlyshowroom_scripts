bf9face74d4d8ba1f4fbd62bee8761bfbe892bca
Phuc Hoang <hoangmaithienphuc@gmail.com>
2023-12-29 22:33:07 +0700

    added DonVi and insert TENTHONGSO script

--- helper_script/insertGIATRITHONGSO.js
+++ helper_script/insertGIATRITHONGSO.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
-const { faker } = require('@faker-js/faker/locale/vi');
-require('dotenv').config();
+const { faker } = require("@faker-js/faker/locale/vi");
+require("dotenv").config();
 
 // Your database configuration
 const dbConfig = {
@@ -24,17 +24,78 @@ connection.connect((err) => {
 
   // Insert data into the database using Faker
   const insertData = () => {
-    const fakeTenKH = faker.person.fullName();
-    const fakeSDT = faker.phone.number();
-    const fakeEmail = faker.internet.email();
-    const fakeTongChiTieu = faker.number.float({ min: 300000000, max: 2000000000 });
+    const rand = faker.number.int({ min: 1, max: 80 });
+    if (rand > 9) {
+      fakeMaXe = "XE00" + rand;
+    } else {
+      fakeMaXe = "XE000" + rand;
+    }
+    const fakeMaTTS = faker.number.int({ min: 1, max: 55 });
+    const fakeGiaTri = faker.number.float({ min: 1, max: 2000 });
+    const fakeDonVi = faker.helpers.arrayElement(
+      // Distance
+      "km",
+      "m",
+      "mi",
+      "yd",
 
-    const query = 'INSERT INTO KHACHHANG (TenKH, SDT, Email, TongChiTieu ) VALUES (?, ?, ?, ?)';
-    const values = [fakeTenKH, fakeSDT, fakeEmail, fakeTongChiTieu];
+      // Speed
+      "km/h",
+      "m/s",
+      "mph",
+      "kn",
+
+      // Time
+      "s",
+      "min",
+      "h",
+
+      // Fuel Efficiency
+      "km/L",
+      "mpg",
+
+      // Mass
+      "kg",
+      "g",
+      "lb",
+
+      // Power
+      "kW",
+      "hp",
+
+      // Torque
+      "Nm",
+      "lb-ft",
+
+      // Temperature
+      "°C",
+      "°F",
+
+      // Pressure
+      "Pa",
+      "kPa",
+      "psi",
+
+      // Volume
+      "L",
+      "mL",
+      "gal",
+
+      // Angle
+      "°",
+      "rad",
+
+      // Percentage
+      "%"
+    );
+
+    const query =
+      "INSERT INTO GIATRITHONGSO (MaXe, MaTTS, GiaTri, DonVi ) VALUES (?, ?, ?, ?)";
+    const values = [fakeMaXe, fakeMaTTS, fakeGiaTri, fakeDonVi];
 
     connection.query(query, values, (err, results) => {
       if (err) {
-        console.error('Error inserting data:', err);
+        console.error("Error inserting data:", err);
         throw err;
       }
       console.log("Data inserted successfully");
@@ -42,10 +103,9 @@ connection.connect((err) => {
     });
   };
 
-
   // Insert data 10 times (you can adjust the number as needed)
   for (let i = 0; i < 100; i++) {
     insertData();
   }
-connection.end();
+  connection.end();
 });
